209f89761540388c92e213feb04744937578fb07
MonicaDeBona <debona.monica@outlook.it>
2023-01-26 17:50:16 +0100

    Create session and moved password result in password.php with redirect

--- index.php
+++ index.php
@@ -13,7 +13,15 @@ Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, l
 Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
 Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
 <?php
-include_once __DIR__ . '/functions.php'
+session_start();
+include_once __DIR__ . '/functions.php';
+if (isset($_GET['userPswLn'])) {
+    $pswLn = $_GET['userPswLn'];
+    $randomPsw = randomPswGenerator($pswLn);
+    $_SESSION['password'] = $randomPsw;
+    header('Location: password.php');
+    exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -22,12 +30,9 @@ include_once __DIR__ . '/functions.php'
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <!-- Importing bootstrap v5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
     <link rel="stylesheet" href="./css/style.css">
-
     <title>Password Generator</title>
 </head>
 
@@ -41,15 +46,6 @@ include_once __DIR__ . '/functions.php'
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
-        </div>
-        <div>
-            <?php
-            if (isset($_GET['userPswLn'])) {
-                $pswLn = $_GET['userPswLn'];
-                $randomPsw = randomPswGenerator($pswLn);
-                echo "<p> La tua password: {$randomPsw} </p>";
-            }
-            ?>
         </div>
     </main>
 </body>
