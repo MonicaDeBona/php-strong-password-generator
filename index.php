db5c11d47e2a58bbaf29dfd291f2e988d711ea27
MonicaDeBona <debona.monica@outlook.it>
2023-01-26 15:52:43 +0100

    Add form and function for generate randomPsw

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<!-- Descrizione
+Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure. L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+Milestone 1
+Creare un form che invii in GET la lunghezza della password.
+Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+Scriviamo tutto (logica e layout) in un unico file index.php
+Milestone 2
+Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file functions.php che includeremo poi nella pagina principale
+Milestone 3 (BONUS)
+Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+Milestone 4 (BONUS)
+Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
+Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,11 +24,42 @@
     <!-- Importing bootstrap v5.3 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="./css/style.css">
+
     <title>Password Generator</title>
 </head>
 
 <body>
-
+    <main>
+        <div class="form-container">
+            <form action="./index.php" method="GET">
+                <div class="mb-3 d-flex align-items-center justify-content-between">
+                    <label class="form-label" for="typeNumber">Password length:</label>
+                    <input type="number" id="typeNumber" class="form-control w-25" name="userPswLn" />
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </div>
+        <div>
+            <?php
+            function randomPswGenerator($length)
+            {
+                $characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}";
+                $randomPsw = '';
+                for ($i = 0; $i < $length; $i++) {
+                    $randomIndex = random_int(0, strlen($characters) - 1);
+                    $randomPsw .= $characters[$randomIndex];
+                }
+                return $randomPsw;
+            }
+            if (isset($_GET['userPswLn'])) {
+                $pswLn = $_GET['userPswLn'];
+                $randomPsw = randomPswGenerator($pswLn);
+                echo "<p> La tua password:  . {$randomPsw} </p>";
+            }
+            ?>
+        </div>
+    </main>
 </body>
 
 </html>
